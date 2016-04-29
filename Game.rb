c79997b59e17735326140c4bbdffa4a9e34ec67c
Luke Kenneth Sutter <Luke.sutter0012@gmail.com>
2016-04-29 15:16:31 -0600

    Main Body

--- Game.rb
+++ Game.rb
@@ -1,2 +1,16 @@
-# First Commit of my Game
-# Second Commit
+require "gosu"
+
+class HelloTestGame < Gosu::Window
+  def initialize (width = 800, height = 600, fullscreen = false)
+    super
+    self.caption = "Hello!!!!!"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+HelloTestGame.new.show
